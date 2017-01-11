ec97a201566a5aac5cb0269b660c8b916d029d95
Alexander Viand <AlexanderViand@student.ethz.ch>
2017-01-11 12:14:04 +0100

    temp

--- src/Test_GPS.cpp
+++ src/Test_GPS.cpp
@@ -97,62 +97,64 @@ int main(int argc, char *argv[])
 
   setTimersOn();
 
-  FHEcontext context(m, p, r);
-  buildModChain(context, /*L=*/levels);
+  FHEcontext* context = new FHEcontext(m, p, r);
+  buildModChain(*context, /*L=*/levels);
   // cout << context << endl;
   // context.zMStar.printout();
   // cout << endl;
 
   cout << "Generating keys and key-switching matrices... " << std::flush;
-  FHESecKey secretKey(context);
-  secretKey.GenSecKey(/*w=*/64);// A Hamming-weight-w secret key
-  addFrbMatrices(secretKey); // compute key-switching matrices that we need
-  add1DMatrices(secretKey); // compute key-switching matrices that we need
-  const FHEPubKey& publicKey = secretKey;
-  cout << "done\n";
+  FHESecKey* secretKey = new FHESecKey(*context);
+  secretKey->GenSecKey(/*w=*/64);// A Hamming-weight-w secret key
+  addFrbMatrices(*secretKey); // compute key-switching matrices that we need
+  add1DMatrices(*secretKey); // compute key-switching matrices that we need
+  const FHEPubKey* publicKey = secretKey;  
 
+  setTimersOff();
+  printAllTimers();
+  cout << "done\n";
   resetAllTimers();
 
-  EncryptedArray ea = *(context.ea);
-  ea.buildLinPolyMat(false);
+  // EncryptedArray ea = *(context.ea);
+  // ea.buildLinPolyMat(false);
 
-  Ctxt ctxt(publicKey);
-  NewPlaintextArray ptxt(ea);
-  random(ea, ptxt);
-  // ea.encrypt(ctxt, publicKey, ptxt);
-  ea.skEncrypt(ctxt, secretKey, ptxt);
+  // Ctxt ctxt(publicKey);
+  // NewPlaintextArray ptxt(ea);
+  // random(ea, ptxt);
+  // // ea.encrypt(ctxt, publicKey, ptxt);
+  // ea.skEncrypt(ctxt, secretKey, ptxt);
 
 
-  cout << "Extracting " << nb_coeffs << " coefficients...";
-  vector<Ctxt> coeffs;
-  extractCoeffs(ea, coeffs, ctxt, nb_coeffs);
-  cout << "done\n";
+  // cout << "Extracting " << nb_coeffs << " coefficients...";
+  // vector<Ctxt> coeffs;
+  // extractCoeffs(ea, coeffs, ctxt, nb_coeffs);
+  // cout << "done\n";
 
-  vector<ZZX> ptxtDec;
-  ea.decrypt(ctxt, secretKey, ptxtDec);
+  // vector<ZZX> ptxtDec;
+  // ea.decrypt(ctxt, secretKey, ptxtDec);
 
-  for (long i=0; i<(long)coeffs.size(); i++) {
-    if (!coeffs[i].isCorrect()) {
-      cerr << " potential decryption error for "<<i<<"th coeffs " << endl;
-      CheckCtxt(coeffs[i], "");
-      exit(0);
-    }
-    vector<ZZX> pCoeffs;
-    ea.decrypt(coeffs[i], secretKey, pCoeffs);
+  // for (long i=0; i<(long)coeffs.size(); i++) {
+  //   if (!coeffs[i].isCorrect()) {
+  //     cerr << " potential decryption error for "<<i<<"th coeffs " << endl;
+  //     CheckCtxt(coeffs[i], "");
+  //     exit(0);
+  //   }
+  //   vector<ZZX> pCoeffs;
+  //   ea.decrypt(coeffs[i], secretKey, pCoeffs);
 
-    assert(pCoeffs.size() == ptxtDec.size());
+  //   assert(pCoeffs.size() == ptxtDec.size());
 
-    for (int j = 0; j < pCoeffs.size(); ++j) {
-      if (coeff(pCoeffs[j], 0) != coeff(ptxtDec[j], i)) {
-        cerr << "error: extracted coefficient " << i << " from " 
-          "slot " << j << " is " << coeff(pCoeffs[j], 0) << " instead of " << 
-          coeff(ptxtDec[j], i) << endl;
-        exit(0);
-      }
-    }
+  //   for (int j = 0; j < pCoeffs.size(); ++j) {
+  //     if (coeff(pCoeffs[j], 0) != coeff(ptxtDec[j], i)) {
+  //       cerr << "error: extracted coefficient " << i << " from " 
+  //         "slot " << j << " is " << coeff(pCoeffs[j], 0) << " instead of " << 
+  //         coeff(ptxtDec[j], i) << endl;
+  //       exit(0);
+  //     }
+  //   }
 
-  }  
-  cerr << "Extracted coefficient successfully verified!\n";
+  // }  
+  // cerr << "Extracted coefficient successfully verified!\n";
 }
 
 
